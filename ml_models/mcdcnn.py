cfe9305b6eaa26b35b8d4d480fb4b3920affbefd
Mehrad <59752677+MehradSm@users.noreply.github.com>
2022-04-17 15:42:31 -0400

    Add files via upload

--- ml_models/mcdcnn.py
+++ ml_models/mcdcnn.py
@@ -77,6 +77,8 @@ def indices(data, function):
 ##################################### Main #####################################
 X_train, y_train, X_test, y_test = preprocessingData()
 
+y_test_copy = y_test
+
 timesteps = X_train.shape[1]
 data_dim = X_train.shape[2]
 
@@ -92,7 +94,7 @@ model = MCDCNN(timesteps, data_dim)
 model.summary()
 
 print('Fitting model...')
-hist = model.fit(X_train, y_train, batch_size=128, epochs=20, validation_split=0.1, verbose=1)
+hist = model.fit(X_train, y_train, batch_size=128, epochs=1, validation_split=0.1, verbose=1)
 
 score, acc = model.evaluate(X_test, y_test, batch_size=10)
 label_ml = model.predict(X_test, batch_size=10)
@@ -101,30 +103,31 @@ print('Test score:', score)
 print('Test accuracy:', acc)
 
 # Test Accuracy by Class
-idx_unf = indices(y_test, lambda x:x==0)
-idx_pos = indices(y_test, lambda x:x==1)
-idx_posspd = indices(y_test, lambda x:x==2)
-idx_posspddir = indices(y_test, lambda x:x==3)
+idx_unf = indices(y_test_copy, lambda x:x==0)
+idx_pos = indices(y_test_copy, lambda x:x==1)
+idx_posspd = indices(y_test_copy, lambda x:x==2)
+idx_posspddir = indices(y_test_copy, lambda x:x==3)
+
+y_test_copy = keras.utils.to_categorical(y_test_copy)
 
 X_test_unf = X_test[idx_unf,:]
 X_test_unf = prepare_input(X_test_unf)
-score_unf, acc_unf = model.evaluate(X_test_unf, y_test[idx_unf,:], batch_size=10)
+score_unf, acc_unf = model.evaluate(X_test_unf, y_test_copy[idx_unf,:], batch_size=10)
 
 X_test_pos = X_test[idx_pos,:]
 X_test_pos = prepare_input(X_test_pos)
-score_pos, acc_pos = model.evaluate(X_test_pos, y_test[idx_pos,:], batch_size=10)
+score_pos, acc_pos = model.evaluate(X_test_pos, y_test_copy[idx_pos,:], batch_size=10)
 
 X_test_posspd = X_test[idx_posspd,:]
 X_test_posspd = prepare_input(X_test_posspd)
-score_posspd, acc_posspd = model.evaluate(X_test_posspd, y_test[idx_posspd,:], batch_size=10)
+score_posspd, acc_posspd = model.evaluate(X_test_posspd, y_test_copy[idx_posspd,:], batch_size=10)
 
 X_test_posspddir = X_test[idx_posspddir,:]
 X_test_posspddir =  prepare_input(X_test_posspddir)
-score_posspddir, acc_posspddir = model.evaluate(X_test_posspddir, y_test[idx_posspddir,:], batch_size=10)
+score_posspddir, acc_posspddir = model.evaluate(X_test_posspddir, y_test_copy[idx_posspddir,:], batch_size=10)
 
 print('Test acc_unf:', acc_unf)
 print('Test acc_pos:', acc_pos)
 print('Test acc_posspd:', acc_posspd)
 print('Test acc_posspddir:', acc_posspddir)
-
 
